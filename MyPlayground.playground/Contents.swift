42ce22dd66a90c36cbfe3d6f9bd1f431b61c1e86
Kevin Youssef <kevinyoussef@Kevins-MacBook-Pro.local>
2019-08-14 11:09:14 +0200

    Initial Push

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+import Foundation
+import XCPlayground
+import PlaygroundSupport
+import UIKit
+
+
+PlaygroundPage.current.needsIndefiniteExecution = true
